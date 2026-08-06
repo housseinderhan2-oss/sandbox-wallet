f46c6ae306a0560485d90378f7d5eff44ad150ca
housseinderhan2-oss <housseinderhan2@gmail.com>
2026-08-06 18:49:55 +0100

    Add MYAMANA API configuration variables
    Added MYAMANA_API_URL and MYAMANA_API_KEY to configuration.

--- config.py
+++ config.py
@@ -9,4 +9,7 @@ class Config:
     HOST = "0.0.0.0"
     PORT = int(os.getenv("PORT", 5000))
     DATABASE_URL = "data_core.db"
-    ENCRYPTION_KEY = os.getenv("K_3", "6fX5Uv9M8K2Pz7pQ4wT1xY3sN5bV8mK0L1rT4wX7zP8=").encode()
+ ENCRYPTION_KEY = os.getenv("K_3", "6fX5Uv9M8K2Pz7pQ4wT1xY3sN5bV8mK0L1rT4wX7zP8=").encode()
+
+Config.MYAMANA_API_URL
+Config.MYAMANA_API_KEY
